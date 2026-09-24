543e48f3fbce654798f0bafe05ad97c435beb6b3
abot8639 <abot8639@gmail.com>
2026-09-24 11:01:43 +0300

    feat: add payment fields to orders table and fcm_token to users table

--- database/migrations/2026_09_24_080112_add_payment_fields_to_orders_and_fcm_token_to_users_table.php
+++ database/migrations/2026_09_24_080112_add_payment_fields_to_orders_and_fcm_token_to_users_table.php
@@ -11,8 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('transaction_id')->nullable()->after('tracking_number')->index();
+            $table->json('payment_details')->nullable()->after('transaction_id');
+        });
+
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('fcm_token')->nullable()->after('remember_token')->index();
         });
     }
 
@@ -21,8 +26,12 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn(['transaction_id', 'payment_details']);
+        });
+
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('fcm_token');
         });
     }
 };
