7da1e9786d59a9dffda18f0e52b63aaf88a3f51e
abot8639 <abot8639@gmail.com>
2026-09-24 11:06:30 +0300

    feat: add Paymob, Stripe, and FCM service configurations and webhook exception

--- config/services.php
+++ config/services.php
@@ -35,4 +35,26 @@ return [
         ],
     ],
 
+    'paymob' => [
+        'api_key' => env('PAYMOB_API_KEY'),
+        'integration_id' => env('PAYMOB_INTEGRATION_ID'),
+        'iframe_id' => env('PAYMOB_IFRAME_ID'),
+        'hmac_secret' => env('PAYMOB_HMAC_SECRET'),
+        'currency' => env('PAYMOB_CURRENCY', 'EGP'),
+        'base_url' => env('PAYMOB_BASE_URL', 'https://accept.paymob.com/api'),
+    ],
+
+    'stripe' => [
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+        'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
+        'currency' => env('STRIPE_CURRENCY', 'usd'),
+    ],
+
+    'fcm' => [
+        'project_id' => env('FIREBASE_PROJECT_ID', 'strongermuscles-8dfd3'),
+        'server_key' => env('FCM_SERVER_KEY'),
+        'credentials_path' => env('FIREBASE_CREDENTIALS_PATH'),
+    ],
+
 ];
